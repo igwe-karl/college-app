21cea7cfad1344ce7412bd7c716969189733c143
igwe-karl <kali4478@gmail.com>
2024-08-14 10:30:11 +0100

    chore: install nextjs and its resources

---
+++ college-app/src/app/invoice/page.tsx
@@ -0,0 +1,13 @@
+import Counter from "../components/counter";
+
+
+const invoice = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default invoice;
---
+++ college-app/src/app/movie/page.tsx
@@ -0,0 +1,11 @@
+//create a hello world
+
+const movie = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-white">
+      <h1 className="text-4xl font-bold text-black">Hello this is a movie page</h1>
+    </div>
+  );
+};
+
+export default movie;
